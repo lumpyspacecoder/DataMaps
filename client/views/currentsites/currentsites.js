11dfafbe5c4ff54949c47580c0c97f879cafdc21
Huy Hoang <hthoang7@uh.edu>
2015-10-09 16:09:13 -0500

    selected points works, now need to finish updating them

--- client/views/currentsites/currentsites.js
+++ client/views/currentsites/currentsites.js
@@ -3,8 +3,9 @@ var chart = null;
 
 function reactiveArea() {
 	  var site =  Session.get("selectedSite");
+         var $report= $('#report');
         Meteor.subscribe('LiveData',site);
-		var ozoneCursor = LiveFeedMonitors.find({siteRef:site}, {limit: 240}, {sort: {epoch: 1} });
+		var ozoneCursor = LiveFeedMonitors.find({siteRef:site}, {limit: 240});
 		var ozoneConDataforGraph = [];
 		ozoneCursor.forEach(function(time) {
 			ozoneConDataforGraph.push({ x: new Date(time.epoch*1000),
@@ -17,13 +18,10 @@ function reactiveArea() {
         }
     });
     
-    chart = $('#container-chart-reactive').highcharts('StockChart', {
-        
-          rangeSelector : {
-                selected : 2,
-              inputEditDateFormat: "%Y-%m-%d-%M-%s",
-              inputDateFormat: "%Y-%m-%d-%M-%s"
-            },
+  var chart = $('#container-chart-reactive').highcharts({
+        chart:{
+            zoomType: 'x'
+        },
         title: {
             text: 'Ozone Readings at ' + site
         },
@@ -34,36 +32,65 @@ function reactiveArea() {
             
         },
         xAxis: {
-            type: 'datetime',
-            ordinal: false
+            type: 'datetime'
         },
         
         yAxis: {
             title: {
                 text: 'Ozone Concentration'
-            },
-            labels: {
-                formatter: function () {
-                    return this.value;
-                }
             }
+//            labels: {
+//                formatter: function () {
+//                    return this.value;
+//                }
+//            }
         },
+      plotOptions: {
+				series: {					
+					turboThreshold: 10000,
+					marker: {
+                    	radius: 2
+                	}
+				}
+			},
         
         series: [{
             name: "Ozone Concentration",
             data: ozoneConDataforGraph,
-            color: '#8CB921',
-            lineWidth : 0,
-            marker : {
-                enabled : true,
-                radius : 2
-            },
-            tooltip: {
-                valueDecimals: 2
+            color: '#8CB921'
+        }],
+        plotOptions: {
+            series: {
+                allowPointSelect: true,
+                point: {
+                    events: {
+                        select: function() {
+                            var selectedPointsStr = "";                            
+                            // when is the chart object updated? after this function finshes?
+                            var chart = this.series.chart;
+                            var selectedPoints = chart.getSelectedPoints();
+                            selectedPoints.push(this);
+                            $.each(selectedPoints, function(i, value) {
+                    			selectedPointsStr += "<br>"+value.category;
+		                    });
+                            
+                            $report.html(selectedPointsStr); 
+                            
+                             // button handler
+                            $('#button').click(function () {
+                                var result = prompt("Enter the updated value for your selection:")
+                                chart.series[0].data[0].update(result);
+                            });
+                          
+                        },
+                        update: function() {}
+                    }
+                }
             }
-        }]
+        }
     });
 
+   
 	}
 
 Template.currentsites.rendered = function () {

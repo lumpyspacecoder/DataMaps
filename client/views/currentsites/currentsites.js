0b8ff5e873d7fb55a92d6419c38e94764748acc0
Price, Daniel M <dprice3@Central.UH.EDU>
2015-11-01 17:31:26 -0600

    not working from waitOn or data in router

--- client/views/currentsites/currentsites.js
+++ client/views/currentsites/currentsites.js
@@ -2,104 +2,12 @@ currentSites = new Meteor.Collection('currentsites');
 
 var selectedPoints = null;
 var ozoneCursor = null;
-	
+
 
 Template.currentsites.onRendered(function (){
 //Template.currentsites.onCreated(function (){
 	//make collections, not vars, and populate from subscription
-	site = new ReactiveVar();
-	time2find = new ReactiveVar();
-	site.set('481670571'); //neet to check about subscribe needing string
-	time2find.set((new Date).getTime());//passing epoch as most recent?
-	time2find.set('5196299900000');  //for testing
-	var timeChosen = time2find.get() - (time2find.get()%36000000);
-	var timeChosenStr = timeChosen.toString().replace(/0+$/,'');
- 
- 	Tracker.autorun(function () {
-		
-		Meteor.subscribe('livedata',site.get(),timeChosenStr);
-		
-		//Meteor.subscribe('livedata','481670571','5196276');
-		pollutCursor = LiveData.find({}, {limit: 240});
-		dataSets = new ReactiveDict();
-		dataPacks = new ReactiveDict();
-		dataIngraph = {};
-	//data4graph.push({x:123,y:432});
-		pollutCursor.forEach(function(line){
-			for (metron in line.subTypes.metrons){
-				if (!dataIngraph[metron]) { //metron is name of measuring quantity
-					dataIngraph[metron] = {};
-					dataIngraph[metron]['Flag'] = [];
-				};
-				for (i=0;i<line.subTypes.metrons[metron].length;i++){
-					var metric = line.subTypes.metrons[metron][i].metric
-					if (metric == 'Flag'){
-						dataIngraph[metron]['Flag'].push({ 
-							x: new Date(line.epoch*1000),
-							y: line.subTypes.metrons[metron][i].val
-						})
-					}else{
-						if (!dataIngraph[metron][metric]){
-							dataIngraph[metron][metric] = [];
-						}else{
-							dataIngraph[metron][metric].push({
-								x: new Date(line.epoch*1000),
-								y: line.subTypes.metrons[metron][i].val
-							})
-						}
-					}
-				}
-			}
-			
-			//console.log('dataIngraph',dataIngraph)
-			// dataIngraph.push({ x: new Date(line.epoch*1000),
-			// 				  y: line.subTypes.metrons.O3[0].val
-			// });
-		});
-		var data4graph = {};
-		for (metron in dataIngraph){
-			data4graph[metron] = {};
-			for (metric in dataIngraph[metron]){
-				data4graph[metron].name = metric;
-				data4graph[metron].color = '#8CB921'; //could choose from an indexed list??
-				data4graph[metron].type = 'bubble';
-				data4graph[metron].data = dataIngraph[metron][metric]
-				//each needs name, color, type, and data
-				//data4graph[metric] = dataIngraph[metron][metric]
-			}
-			//console.log('data4graph',data4graph)
-			dataSets.set(metron,data4graph)
-			//console.log(dataSets.get(metron))
-			//console.log(metron,dataIngraph[metron]) 
-			
-			//console.log(metron,metric,dataIngraph[metron][metric])
-		};
-		// data4graph.data = //dataIngraph; //could make the others into objects, too -http://api.highcharts.com/highcharts#series<areaspline>
-		// data4graph.name = 'Overall Graph';
-		// data4graph.color = '#8CB921';
-		// data4graph.type = 'bubble';
-		// dataSets.set('data4graph',data4graph) //title .toString, then the graphdata stuff???
-		// flag4graph = {};
-		// flag4graph.data = flagIngraph;
-		// flag4graph.color = 'pink'; //make opaque?
-		// flag4graph.type = 'bar';
-		var dataSeries = function(){
-			//console.log(dataSets.keys)
-			for (key in dataSets.keys){
-//				console.log('data',dataSets.get(key)[key].data)
-				console.log('key',key) //key is the name of the metric
-				console.log(dataSets.get(key)[key].name)
-				console.log(dataSets.get('O3')['O3'])
-				//key is the metron, and should be used for higher on select
- 				return {name: dataSets.get(key)[key].name,
-						data: dataSets.get(key)[key].data, //dataSets.keys[key],
-						color: dataSets.get(key)[key].color,
-						type: dataSets.get(key)[key].type
-				}
-			}
-		}
-		
-		console.log('dataSeries',dataSeries())
+ //	Tracker.autorun(function () {
 		//can we put five minute with box plots; other pollutants; delete, etc.
 		var $report= $('#report');
 		Highcharts.setOptions({
@@ -183,7 +91,7 @@ Template.currentsites.onRendered(function (){
 		        }
 		    }
 	}); //end of chart
-});//end autorun
+//});//end autorun
 	//var data4graphColl = new Meteor.Collection('dataInGraph');
 }); //end of onRendered
 /*
--- client/views/routing.js
+++ client/views/routing.js
@@ -16,7 +16,75 @@ Router.route('/',{
 Router.route('/currentsites', {
 	name:'currentsites',
 	template:'currentsites',
-	data: function(){
+	waitOn: function(){
+        site = new ReactiveVar();
+        time2find = new ReactiveVar();
+        site.set('481670571'); //neet to check about subscribe needing string
+        time2find.set((new Date).getTime());//passing epoch as most recent?
+        time2find.set('5194882710000');  //for testing
+        timeChosen = time2find.get() - (time2find.get()%360000000);
+        timeChosenStr = timeChosen.toString().replace(/0+$/,'');
+        Meteor.subscribe('livedata',site.get(),timeChosenStr);
+		pollutCursor = LiveData.find({}, {limit: 240});
+        //console.log('cnt',pollutCursor.count())
+		dataSets = new ReactiveDict();
+		dataPacks = new ReactiveDict();
+		dataIngraph = {};
+		pollutCursor.forEach(function(line){
+            //console.log(line)
+			for (metron in line.subTypes.metrons){
+				if (!dataIngraph[metron]) { //metron is name of measuring quantity
+					dataIngraph[metron] = {};
+					dataIngraph[metron]['Flag'] = [];
+				};
+				for (i=0;i<line.subTypes.metrons[metron].length;i++){
+					var metric = line.subTypes.metrons[metron][i].metric
+					if (metric == 'Flag'){
+						dataIngraph[metron]['Flag'].push({ 
+							x: new Date(line.epoch*1000),
+							y: line.subTypes.metrons[metron][i].val
+						})
+					}else{
+						if (!dataIngraph[metron][metric]){
+							dataIngraph[metron][metric] = [];
+						}else{
+							dataIngraph[metron][metric].push({
+								x: new Date(line.epoch*1000),
+								y: line.subTypes.metrons[metron][i].val
+							})
+						}
+					}
+				}
+			}
+		});
+		var data4graph = {};
+		for (metron in dataIngraph){
+			data4graph[metron] = {};
+			for (metric in dataIngraph[metron]){
+				data4graph[metron].name = metric;
+				data4graph[metron].color = '#8CB921'; //could choose from an indexed list??
+				data4graph[metron].type = 'bubble';
+				data4graph[metron].data = dataIngraph[metron][metric]
+			}
+			dataSets.set(metron,data4graph)
+		};
+		dataSeries = function(){
+			//console.log(dataSets.keys)
+			for (key in dataSets.keys){
+//				console.log('data',dataSets.get(key)[key].data)
+//				console.log('key',key) //key is the name of the metric
+//				console.log(dataSets.get(key)[key].name)
+//				console.log(dataSets.get('O3')['O3'])
+				//key is the metron, and should be used for higher on select
+ 				return {name: dataSets.get(key)[key].name,
+						data: dataSets.get(key)[key].data, //dataSets.keys[key],
+						color: dataSets.get(key)[key].color,
+						type: dataSets.get(key)[key].type
+				}
+			}
+		}
+		
+		//console.log('dataSeries',dataSeries())
 	},
 	action: function () {
 		if (this.ready()) {

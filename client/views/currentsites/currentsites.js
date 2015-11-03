813dbce0a894f9362f5660a618f8bad254c46a01
Price, Daniel M <dprice3@Central.UH.EDU>
2015-11-03 15:52:04 -0600

    two on same - still need zoom and update/remove

--- client/views/currentsites/currentsites.js
+++ client/views/currentsites/currentsites.js
@@ -1,157 +1,14 @@
 currentSites = new Meteor.Collection('currentsites');
 
 var selectedPoints = null;
-Template.currentsites.onRendered(function (){
-	//make collections, not vars, and populate from subscription
-	//if you start autorun here, it reloads on refresh, but does all the calculations 4-8 times
-		//can we put five minute with box plots; other pollutants; delete, etc.
-        site = new ReactiveVar();
-        time2find = new ReactiveVar();
-        site.set('481670571'); //neet to check about subscribe needing string
-        time2find.set((new Date).getTime());//passing epoch as most recent?
-        time2find.set('1446444488');  //for testing 5196299900000 (uh)/5196294320000 /laptop
-        timeChosen = time2find.get() - (time2find.get()%1000000);
-        timeChosenStr = timeChosen.toString().replace(/0+$/,'');
-        Meteor.subscribe('livedata',site.get(),timeChosenStr);
-		pollutCursor = LiveData.find({}, {limit: 40});
-		dataSets = new ReactiveDict();
-		dataPacks = new ReactiveDict();
-		dataIngraph = {};
-		pollutCursor.forEach(function(line){
-			for (metron in line.subTypes.metrons){
-				if (!dataIngraph[metron]) { //metron is name of measuring quantity
-					dataIngraph[metron] = {};
-					dataIngraph[metron]['Flag'] = [];
-				};
-				for (i=0;i<line.subTypes.metrons[metron].length;i++){
-					var metric = line.subTypes.metrons[metron][i].metric
-//					if (metric == 'Flag'){
-//						dataIngraph[metron]['Flag'].push({ 
-//							x: new Date(line.epoch*1000),
-//							y: line.subTypes.metrons[metron][i].val
-//						})
-//					}else{
-						if (!dataIngraph[metron][metric]){
-							dataIngraph[metron][metric] = [];
-						}else{
-							dataIngraph[metron][metric].push({
-								x: new Date(line.epoch*1000),
-								y: line.subTypes.metrons[metron][i].val
-							})
-						}
-				//	}
-				}
-			}
-		});
-//	Tracker.autorun(function () {
-		selectData = new ReactiveVar();
-		var data4graph = {};
-		for (metron in dataIngraph){
-			var data4select = [];
-			data4graph[metron] = {};
-			for (metric in dataIngraph[metron]){
-				data4graph[metron].name = metron+'_'+metric;
-				data4graph[metron].color = '#8CB921'; //could choose from an indexed list??
-				data4graph[metron].type = 'bubble';
-				data4graph[metron].data = dataIngraph[metron][metric];
-				dataSets.set(metron+'_'+metric,data4graph[metron]);
-				data4select.push(metric);
-			}
-			dataPacks.set(metron,data4select);
-			selectData.set(data4select);
-		};
-    Tracker.autorun(function () {
-            
-		dataSeriesVar = new ReactiveVar();
-		dataSeries = function(metron){
-			return dataSets.get(metron)
-		};
-		dataSeriesVar.set([]);//dataSeries('O3_conc');chart.series[0].setData([dataSeriesVar]);
-		//or//chart.series[0].setData([dataSeries(metron)]);
-		
-		var $report= $('#report');
-		Highcharts.setOptions({
-		    global: {
-		        useUTC: false
-		    }
-		});
-		var chart = $('#container-chart-reactive').highcharts({
-		    exporting: {
-		        chartOptions: { // specific options for the exported image
-		            plotOptions: {
-		                series: {
-		                    dataLabels: {
-		                        enabled: true
-		                    }
-		                }
-		            }
-		        },
-		        scale: 3,
-		        fallbackToExportServer: false
-		    },
-		    chart:{
-				events: {
-					//click: function(){alert('dafdfs')},
-					selection: function(event) {
-						for (var i = 0; i < this.series[0].data.length; i++) {
-							var point = this.series[0].data[i];
-							if (point.x > event.xAxis[0].min &&
-								point.x < event.xAxis[0].max &&
-								point.y > event.yAxis[0].min &&
-								point.y < event.yAxis[0].max) {
-									point.select(true, true);
-								}
+Template.currentsites.onCreated(function (){
 
-						}
-						return false;
-					}
-				},
-				zoomType: 'xy'
-		    },
-		    title: {
-		        text: 'Ozone Readings at ' + site
-		    },
-		    credits: {
-		        text: "UH-HNET",
-		        href: "http://hnet.uh.edu"
+});
+Template.currentsites.onRendered(function (){
+//    Tracker.autorun(function () {
+            
 		
-		    },
-		    xAxis: {
-		        type: 'datetime'
-		    },
-		    yAxis: {
-		        title: {
-		            text: 'Ozone Concentration'
-		        }
-		    },
-			series: [dataSeriesVar],
-		     // series: [{
-		     //     name: "Ozone Concentration",
-		     //     data: [],//dataSets.get('O3')['03'].data,//data4graph,
-		     //     color: '#8CB921'
-		     // }],
-		    plotOptions: {
-		        series: {
-		            allowPointSelect: true,
-		            point: {
-		                events: {
-		                    select: function() {
-		                        var selectedPointsStr = "";
-		                        // when is the chart object updated? after this function finshes?
-		                        var chart = this.series.chart;
-		                        selectedPoints = chart.getSelectedPoints();
-		                        selectedPoints.push(this);
-		                        $.each(selectedPoints, function(i, value) {
-		                			selectedPointsStr += "<br>"+value.category;
-				                    });
-		                        $report.html(selectedPointsStr);
-		                    }
-		                }
-		            }
-		        }
-		    }
-	}); //end of chart
-});//end autorun
+//});//end autorun
 	//var data4graphColl = new Meteor.Collection('dataInGraph');
 }); //end of onRendered
 
@@ -167,12 +24,17 @@ Template.currentsites.helpers({
 	
 });
 Template.currentsites.events({
+    "change #timeselect": function(){
+        dataSeriesVar.set(dataSeries('O3_conc'));
+                        },
+    "click #packselect": function(){
+                        },
 	"change #packselect": function(event){
-		dataSeriesVar.set(dataSeries(event.currentTarget.value)) //should be the metron_metric combo
+		dataSeriesVar.set('O3_'+event.currentTarget.value) //should be the metron_metric combo
 		 //Template.instance().ctrlMenus.set('collectName', event.currentTarget.value); if works in onCreated
 	},
 	"change #keyselect": function(event){
-		alert('dfa')
+		dataSeriesVar.set(dataSeries(event.currentTarget.value))
 		//dataSeriesVar.set(dataSeries(event.currentTarget.value)) //should be the metron_metric combo
 		 //Template.instance().ctrlMenus.set('collectName', event.currentTarget.value); if works in onCreated
 	},
@@ -212,28 +74,4 @@ Template.currentsites.events({
         });
     }
 
-});
-
-Template.menu.events({
-    // depending on which site the user clicks to learn more about, session variable will be changed and passed to currentsites.js
-    'change #siteselect': function(e) {
-        var newValue = $(e.target).val();
-        Session.set("selectedSite", newValue);
-    }
-});
-
-Template.menu.onCreated( function() {
-    // creation of reactive var which will be a mongo query for the menu of live data monitors
-    var self = this;
-    self.autorun(function () {
-    self.subscribe('LiveMenu');
-  });
-});
-
-Template.menu.helpers({
-      currentSites: function () {
-        var data = LiveData.find().fetch();
-         var distinctData = _.uniq(data, false, function(d) {return d.siteRef});
-         return _.pluck(distinctData, "siteRef");
-}
 });
--- server/publication.js
+++ server/publication.js
@@ -1,43 +1,45 @@
+//This works, and should make it easier/faster but I wasn't using the functions
+//Meteor.publish('livedata', function (site,timeChosen) {
+//	var self = this; //because 'this' is different inside of added
+//	var siteTimeChosen = new RegExp('^'+site+'_'+timeChosen);
+//	var live2show = LiveData.find({_id: {$regex:siteTimeChosen}}).observeChanges({
+//		added: function (id, fields) {
+//			self.added("livedata", id, fields);
+//			// self.added("dataInGraph",id, {
+//			// x: new Date(fields.epoch*1000),
+//			// y: fields.subTypes.metrons.O3[0].val
+//			// 	});
+//		//	console.log('added', id, fields)
+//		//	console.log('LiveDatacount',LiveData.find().count())
+//		},
+//		changed: function(id, fields) {
+//		  self.changed("livedata", id, fields);
+//		  //can't quite tell if it automatically calls an update to the db
+//		},
+//		removed: function (id) {
+//			console.log('removed')
+//			console.log('LiveDatacount',LiveData.find().count())
+//		  self.removed("livedata", id);
+//		}
+//	});
+//	// self.added("currentsites", id, site);
+//	// self.added("dataInGraph", id, {x: "testdata", Count: count});
+//	self.ready();
+////	console.log(this.connection) //curious about who is connecting? could log, I guess...
+//	self.onStop(function () {
+//		live2show.stop();
+//	});
+//	//return LiveData.find({_id: {$regex:siteTimeChosen}});
+//});
 Meteor.publish('livedata', function (site,timeChosen) {
-	var self = this; //because 'this' is different inside of added
+    console.log(timeChosen);
 	var siteTimeChosen = new RegExp('^'+site+'_'+timeChosen);
-	var live2show = LiveData.find({_id: {$regex:siteTimeChosen}}).observeChanges({
-		added: function (id, fields) {
-			self.added("livedata", id, fields);
-			// self.added("dataInGraph",id, {
-			// x: new Date(fields.epoch*1000),
-			// y: fields.subTypes.metrons.O3[0].val
-			// 	});
-		//	console.log('added', id, fields)
-		//	console.log('LiveDatacount',LiveData.find().count())
-		},
-		changed: function(id, fields) {
-		  self.changed("livedata", id, fields);
-		  //can't quite tell if it automatically calls an update to the db
-		},
-		removed: function (id) {
-			console.log('removed')
-			console.log('LiveDatacount',LiveData.find().count())
-		  self.removed("livedata", id);
-		}
-	});
-	// self.added("currentsites", id, site);
-	// self.added("dataInGraph", id, {x: "testdata", Count: count});
-	self.ready();
-//	console.log(this.connection) //curious about who is connecting? could log, I guess...
-	self.onStop(function () {
-		live2show.stop();
-	});
-	//return LiveData.find({_id: {$regex:siteTimeChosen}});
-});
-/* works first time, not refresh browser
-Meteor.publish('livedata', function (site,timeChosen) {
-	var siteTimeChosen = new RegExp('^'+site+'_'+timeChosen);
-	console.log('siteTimeChosen in publish',siteTimeChosen)
-	console.log(LiveData.find({_id: {$regex:siteTimeChosen}}).count())
 	return LiveData.find({_id: {$regex:siteTimeChosen}});
 });
-*/
+Meteor.publish('aggregatedata5min', function (site,timeChosen) {
+	var siteTimeChosen = new RegExp('^'+timeChosen);
+	return AggrData.find({});
+});
 Meteor.publish('tceqData', function () {
     var now = new Date();
     var adayAgo = now.getTime() / 1000 - 24 * 3600;
